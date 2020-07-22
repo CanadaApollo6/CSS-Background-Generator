2b556159b4b6bf268ca7c2b1a0f1f709057b740d
Riel St. Amand <riel.stamand@gmail.com>
2020-07-22 17:24:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
-css.textContent = body.style.background + ";";
-
 function setGradient() {
 	body.style.background = 
 	"linear-gradient(to right, " 
@@ -15,6 +13,8 @@ function setGradient() {
 
 	css.textContent = body.style.background + ";";
 }
+
+setGradient();
 
 color1.addEventListener("input", setGradient);
 
